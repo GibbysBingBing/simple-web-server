4d59250c5c9f8a0b03f9a202101868eb3f3e0bb2
Dylan Gibbons <dylanagibbons@gmail.com>
2022-10-17 14:36:17 +0100

    Added static assets (html, css and javascript) to a static 'web' folder

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app     = express();
 const port    = 8080;
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+app.use(express.static('web'));
 
 app.listen(port, console.log(`listening on port ${port}`));
